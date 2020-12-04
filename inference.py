fc50ee838c91ac9e5ed2dd72475f3a74541a1cf0
Addrick <asantos@wpi.edu>
2020-12-03 20:14:40 -0500

    created functions within autoencoder.py including data loading so everything can be run piecewise from there

--- inference.py
+++ inference.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 # black and white
-bw_img = img.reshape(1,75,284,1)
+bw_img = img.reshape(1,284,75,1)
 pred = model.predict(bw_img)
 pred *= 255/pred.max()
 pred = pred.astype(np.uint8)
-pred = pred.reshape(75,284)
+# pred = pred.reshape(75,284)
+plt.imshow(img, 'gray')
 plt.imshow(pred, 'gray')
 plt.show()
